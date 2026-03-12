dbe520cc0e714bf8a448e8ce23f4af2b79ec1b03
Seb1721 <seblegaspi16@gmail.com>
2026-03-12 13:14:23 -0700

    fix: made accessible to aws

--- app_backend.py
+++ app_backend.py
@@ -122,4 +122,4 @@ def add_service_route():
         return jsonify({"error": str(e)}), 400
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
