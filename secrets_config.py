b9971c473c006cde4d62c50e4b7be7e29cec0d59
Seb1721 <seblegaspi16@gmail.com>
2026-04-07 17:47:59 -0700

    fix: fix minor error in aws region

--- secrets_config.py
+++ secrets_config.py
@@ -8,7 +8,7 @@ def get_mongodb_uri():
         return local_uri
 
     secret_name = "prod/carkeeper/mongodb"
-    region_name = "us-west-2"
+    region_name = "us-west-1"
 
     client = boto3.client("secretsmanager", region_name=region_name)
     response = client.get_secret_value(SecretId=secret_name)
